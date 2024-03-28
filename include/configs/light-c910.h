e0247b8a622a08256bb876fe706eedd3d5d645f7
Han Gao <gaohan@iscas.ac.cn>
2024-03-28 11:00:40 +0000

    fix: run gpt_partition failed
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- include/configs/light-c910.h
+++ include/configs/light-c910.h
@@ -154,12 +154,15 @@
 	"kdump_buf=180M\0" \
 	"mmcbootpart=2\0" \
 	"default_mmcdev=1\0" \
+	"emmc_dev=0\0" \
+	"sdcard_dev=1\0" \
 	"mmc_select=if test -e mmc ${default_mmcdev}:${mmcbootpart} ${boot_conf_file}; then mmcdev=1; else mmcdev=0; fi;\0" \
 	"boot_conf_file=/extlinux/extlinux.conf\0" \
 	"uuid_rootfsA=80a5a8e9-c744-491a-93c1-4f4194fd690a\0" \
 	"uuid_swap=5ebcaaf0-e098-43b9-beef-1f8deedd135e\0" \
 	"partitions=name=table,size=2031KB;name=boot,size=500MiB,type=boot;name=swap,size=4096MiB,type=swap,uuid=${uuid_swap};name=root,size=-,type=linux,uuid=${uuid_rootfsA}\0" \
-	"gpt_partition=gpt write mmc ${mmcdev} $partitions\0" \
+	"gpt_partition=gpt write mmc ${emmc_dev} $partitions\0" \
+	"sdcard_gpt_partition=gpt write mmc ${sdcard_dev} $partitions\0" \
 	"load_aon=load mmc ${mmcdev}:${mmcbootpart} $fwaddr light_aon_fpga.bin;cp.b $fwaddr $aon_ram_addr $filesize\0" \
 	"load_c906_audio=load mmc ${mmcdev}:${mmcbootpart} $fwaddr light_c906_audio.bin;cp.b $fwaddr $audio_ram_addr $filesize\0" \
 	"load_str=load mmc ${mmcdev}:${mmcbootpart} $fwaddr str.bin;cp.b $fwaddr $str_ram_addr $filesize\0" \
