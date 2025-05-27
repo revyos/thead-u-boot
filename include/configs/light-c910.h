0028a957e3684e6ed2a1f446034191b5120fd3ec
Yao Zi <ziyao@disroot.org>
2025-05-27 01:21:17 +0000

    configs: light-c910: Setup kernel decompression buffer
    Image decompression requires a temporary buffer to store decompressed
    image. Setup it for C910 boards.
    
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- include/configs/light-c910.h
+++ include/configs/light-c910.h
@@ -166,6 +166,8 @@
 	"emmc_dev=0\0" \
 	"sdcard_dev=1\0" \
 	"mmc_select=if test -e ${boottype} ${default_mmcdev}:${mmcbootpart} ${boot_conf_file}; then mmcdev=1; else mmcdev=0; fi;\0" \
+	"kernel_comp_addr_r=0x20000000\0" \
+	"kernel_comp_size=0x08000000\0" \
 	"boot_conf_file=/extlinux/extlinux.conf\0" \
 	"uuid_rootfsA=80a5a8e9-c744-491a-93c1-4f4194fd690a\0" \
 	"uuid_swap=5ebcaaf0-e098-43b9-beef-1f8deedd135e\0" \
